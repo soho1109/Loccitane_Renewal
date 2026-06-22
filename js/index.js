181581302ca3ed1f94b83e43841a636ade99de85
soho1109 <park1109pp@naver.com>
2026-06-22 21:33:40 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,53 @@
+/*스와이퍼*/ 
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".best-sellers-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    on: {
+      init() {
+        updateProgress(this);
+      },
+
+      slideChange() {
+        updateProgress(this);
+      },
+    },
+  });
+
+  function updateProgress(swiper) {
+    const progressFill = document.querySelector(
+      ".best-sellers-progress-fill"
+    );
+
+    const totalSlides = swiper.slides.length;
+    const currentIndex = swiper.activeIndex;
+
+    const barWidth = 100 / totalSlides;
+    const moveX = currentIndex * 100;
+
+    progressFill.style.width = `${barWidth}%`;
+    progressFill.style.transform = `translateX(${moveX}%)`;
+  }
+});
+
+/*스크롤 시 헤더 고정*/ 
+document.addEventListener("DOMContentLoaded", () => {
+  const header = document.querySelector(".header");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      header.classList.add("is-sticky");
+    } else {
+      header.classList.remove("is-sticky");
+    }
+  });
+});
+
+window.addEventListener("load", () => {
+  const bannerTitle = document.querySelector(
+    ".banner-title img"
+  );
+
+  bannerTitle.classList.add("show");
+});
